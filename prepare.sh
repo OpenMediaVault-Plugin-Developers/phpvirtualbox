7441820265befa15fe5f5b5ee723e22459211ae6
Simon Jonsson <nohatssir@users.noreply.github.com>
2014-08-05 15:13:49 +0200

    install vboxinit

--- prepare.sh
+++ prepare.sh
@@ -2,12 +2,13 @@
 
 PHPVIRTUALBOX_VERSION=4.3-1
 
-if [ ! -d phpvirtualbox ]; then
+if [ ! -d src ]; then
     wget http://sourceforge.net/projects/phpvirtualbox/files/phpvirtualbox-${PHPVIRTUALBOX_VERSION}.zip/download -O phpvirtualbox.zip
     unzip phpvirtualbox.zip
-    mv phpvirtualbox-${PHPVIRTUALBOX_VERSION} phpvirtualbox
-    /bin/rm -f phpvirtualbox.zip
-    find . -maxdepth 1 -name "phpvirtualbox-*" -type d -exec /bin/mv {} phpvirtualbox \;
+    mv phpvirtualbox-${PHPVIRTUALBOX_VERSION} src
+    rm -f phpvirtualbox.zip
+    mv src/vboxinit debian/vboxinit.init
+    chmod +x debian/vboxinit.init
 fi
 
 exit 0
