3baf9ae62cc933589203c4c9e48ee2963dfdffc0
Aaron Murray <plugins@omv-extras.org>
2015-04-04 15:45:56 -0500

    update to 4.3-3

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PHPVIRTUALBOX_VERSION=4.3-2
+PHPVIRTUALBOX_VERSION=4.3-3
 
 if [ ! -d src ]; then
     wget http://sourceforge.net/projects/phpvirtualbox/files/phpvirtualbox-${PHPVIRTUALBOX_VERSION}.zip/download -O phpvirtualbox.zip
