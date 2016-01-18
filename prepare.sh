1b77f88f3a8e84989097dc5c56e53e9bd1b5b9aa
Aaron Murray <plugins@omv-extras.org>
2016-01-18 07:47:39 -0600

    update for 5.0-5

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PHPVIRTUALBOX_VERSION=5.0-3
+PHPVIRTUALBOX_VERSION=5.0-5
 
 if [ ! -d src ]; then
     wget http://sourceforge.net/projects/phpvirtualbox/files/phpvirtualbox-${PHPVIRTUALBOX_VERSION}.zip/download -O phpvirtualbox.zip
